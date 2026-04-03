7d79dd443eb6017c4eeeac650962497d8c1883a3
Abhishek yadav <studyandrotech@gmail.com>
2026-04-04 00:04:13 +0530

    git igore

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const authRouter = require("./routes/authRouter")
 const cookieParser = require("cookie-parser")
 const connectDB = require("./config/mongodb")
 const userRouter = require("./routes/userRouter")
+const path = require("path")
 const app = express()
 const port = process.env.PORT || 5000
-
 const allowedOrigins =["http://localhost:5173"]
 
 app.use(express.json())
@@ -20,6 +20,13 @@ app.use(cors({origin:allowedOrigins,
 
 app.use("/api/auth",authRouter)
 app.use("/api/user",userRouter)
+app.use(express.static(path.join(__dirname, "../client/dist")));
+
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+});
+
+
 
 app.get("/",(req,res)=>{
     res.send("API WORKING")
